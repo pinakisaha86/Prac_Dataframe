b220e89ee8977316b6065f7415f4f4a98462c0f3
pinaki Saha <pinaki.saha86@gmail.com>
2022-05-09 21:33:47 +0530

    commit

---
+++ create_emr_cluster.py
@@ -0,0 +1,61 @@
+import boto3
+
+
+def lambda_handler(event, context):
+
+    client = boto3.client('emr',  region_name="eu-west-1")
+
+    instances = {
+        'MasterInstanceType': 'm3.xlarge',
+        'SlaveInstanceType': 'm3.xlarge',
+        'InstanceCount': 2,
+        'InstanceGroups': [],
+        'Ec2KeyName': 'spark',
+        'KeepJobFlowAliveWhenNoSteps': True,
+        'TerminationProtected': False,
+        'Ec2SubnetId': 'subnet-fcb5b5b4',
+        'EmrManagedMasterSecurityGroup': 'sg-089e0747d040803bf',
+        'EmrManagedSlaveSecurityGroup':  'sg-02b23cae880e70d33'
+    }
+
+    configurations = [
+        {
+            'Classification': 'yarn-site',
+            'Properties': {
+                'yarn.resourcemanager.scheduler.class': 'org.apache.hadoop.yarn.server.resourcemanager.scheduler.fair.FairScheduler'
+            },
+            'Configurations': []
+        },
+        {
+            "Classification": "spark-env",
+            "Configurations": [
+                {
+                    "Classification": "export",
+                    "Properties": {
+                        "PYSPARK_PYTHON": "/usr/bin/python3"
+                    }
+                }
+            ]
+        }
+    ]
+
+    response = client.run_job_flow(
+        Name='PySpark Cluster',
+        LogUri='s3://pinaki-bigdata/emr-logs',
+        ReleaseLabel='emr-5.30.0',
+        Instances=instances,
+        Configurations=configurations,
+        Steps=[],
+        BootstrapActions=[],
+        Applications=[
+            {'Name': 'Spark'},
+            {'Name': 'Zeppelin'},
+            {'Name': 'Ganglia'}
+        ],
+        VisibleToAllUsers=True,
+        ServiceRole='EMR_DefaultRole',
+        JobFlowRole='EMR_EC2_DefaultRole',
+        AutoScalingRole='EMR_AutoScaling_DefaultRole',
+        EbsRootVolumeSize=30
+    )
+    return response["JobFlowId"]
---
+++ rdd/scholaship_recipient_join_filter.py
@@ -0,0 +1,59 @@
+from pyspark.sql import SparkSession
+from distutils.util import strtobool
+import os.path
+import yaml
+
+if __name__ == '__main__':
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("RDD examples") \
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    current_dir = os.path.abspath(os.path.dirname(__file__))
+    app_config_path = os.path.abspath(current_dir + "/../" + "application.yml")
+    app_secrets_path = os.path.abspath(current_dir + "/../" + ".secrets")
+
+    conf = open(app_config_path)
+    app_conf = yaml.load(conf, Loader=yaml.FullLoader)
+    secret = open(app_secrets_path)
+    app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Setup spark to use s3
+    hadoop_conf = spark.sparkContext._jsc.hadoopConfiguration()
+    hadoop_conf.set("fs.s3a.access.key", app_secret["s3_conf"]["access_key"])
+    hadoop_conf.set("fs.s3a.secret.key", app_secret["s3_conf"]["secret_access_key"])
+
+    demographics_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/demographic.csv")
+    finances_rdd = spark.sparkContext.textFile("s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/finances.csv")
+
+    demographics_pair_rdd = demographics_rdd \
+        .map(lambda line: line.split(",")) \
+        .map(lambda lst: (int(lst[0]), (int(lst[1]), strtobool(lst[2]), lst[3], lst[4], strtobool(lst[5]), strtobool(lst[6]), int(lst[7]))))
+
+    print('demographic data..')
+    for rec in demographics_pair_rdd.collect():
+        print(rec)
+
+
+    finances_pair_rdd = finances_rdd \
+        .map(lambda line: line.split(",")) \
+        .map(lambda lst: (int(lst[0]), (strtobool(lst[1]), strtobool(lst[2]), strtobool(lst[3]), int(lst[4]))))
+
+    join_pair_rdd = demographics_pair_rdd \
+        .join(finances_pair_rdd)
+
+    print('joined rdds,')
+    for rec in join_pair_rdd.collect():
+        print(rec)
+
+    join_pair_rdd = join_pair_rdd.filter(lambda rec: (rec[1][0][2] == "Switzerland") and (rec[1][1][0] == 1) and (rec[1][1][1] == 1))
+
+    #join_pair_rdd.foreach(print)
+    print('Participants belongs to \'Switzerland\', having debts and financial dependents,')
+    for rec in join_pair_rdd.collect():
+        print(rec)
+
+# spark-submit --packages "org.apache.hadoop:hadoop-aws:2.7.4" rdd/scholaship_recipient_join_filter.py
